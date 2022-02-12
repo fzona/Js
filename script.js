cc2dbc09e2eb22b741760011ef31e78247bb88d8
Federico Zona Le Fort <fzonalefort@gmail.com>
2022-02-12 20:38:50 -0300

    Agregue el DOM y tabla de acciones

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-////Declaro un array vacío de acciones en un portafolio
-const portafolio = [];
+let portafolio = [];
+const formulario = document.getElementById("formulario");
+const nombre = document.getElementById("nombreAccion");
+const costo = document.getElementById("costo");
+const precio = document.getElementById("precio");
+const tabla = document.getElementById("tabla");
 
-//Defino la clase constructora de Accion que recibirá como parámetros nombre, costo, precio, rendimiento
-class Accion {
-  constructor(accion) {
-    this.nombre = accion.nombre;
-    this.costo = accion.costo;
-    this.precio = accion.precio;
-    this.rendimiento = (((this.precio / this.costo) - 1) * 100).toFixed(2) + "%";
-  }
+formulario.addEventListener("submit", function (e){
+  e.preventDefault();
+
+  portafolio.push({
+    nombre: nombre.value,
+    costo: costo.value,
+    precio: precio.value,
+    rendimiento: (((precio.value / costo.value) - 1) * 100).toFixed(2) + "%"
+  });
+  tabla.innerHTML= `  <thead>
+  <tr>
+    <th scope="col">Nombre</th>
+    <th scope="col">Precio de compra</th>
+    <th scope="col">Cotización del día</th>
+    <th scope="col">Rendimiento</th>
+  </tr>
+</thead>`;
+for (const accion of portafolio){
+  let fila = document.createElement("tr");
+  fila.innerHTML = `<td>${accion.nombre}</td>
+  <td>${accion.costo}</td>
+  <td>${accion.precio}</td>
+  <td>${accion.rendimiento}</td>`
+  tabla.appendChild(fila);
 }
-
-////Le pido al usuario que ingrese una accion al portafolio. Si la respuesta es si, obtengo las variables nuevoNombre, nuevoCosto.... para pasarlos como parámetros a mi clase constructora y realizar el método push en el array portafolio.
-let entrada = prompt("Quieres agregar una acción a tu portafolio? (Si o No)");
-
-while (entrada === "Si") {
-  let nuevoNombre = prompt("Ingresa el nombre de la acción");
-  let nuevoCosto = parseInt(prompt("Ingresa a cuanto la compraste"));
-  let nuevoPrecio = parseInt(prompt("A que valor cerró el día de hoy?"));
-  let accionNueva = new Accion({
-    nombre: nuevoNombre,
-    costo: nuevoCosto,
-    precio: nuevoPrecio
-  }); //Utilizando los prompts ingreso el nuevo objeto a la array
-  portafolio.push(accionNueva);
-  entrada = prompt("Quieres agregar una nueva acción a tu portafolio? (Si o No)");
 }
-
-//Utilizo un sort para que reordene la array por la letra incial del nombre de la acción
-portafolio.sort((a, b) => {
-  if (a.nombre > b.nombre) {
-    return 1;
-  }
-  if (a.nombre < b.nombre) {
-    return -1;
-  }
-  // a es igual a b
-  return 0;
-})
-
+)
 console.log(portafolio);
