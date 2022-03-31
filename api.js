f85b563a4773f67917b87130a7695c9939c5c6fd
Federico Zona Le Fort <fzonalefort@gmail.com>
2022-03-31 18:52:08 -0300

    Arreglo de la tabla, falta cargar cuando submitea

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const search = document.getElementById("search");
 const searchAccion = document.getElementById("searchAccion");
 const contenedor = document.getElementById("contenedor");
+const results = document.getElementById("results");
 
 /*mediante un evento cuando el usuario busca una accion de la base de acciones posibles, 
 se hace un fetch a la api de alphavantage que le devuelve el precio final del día y el porcentaje de variación*/
@@ -8,14 +9,10 @@ search.addEventListener("submit", (e) => {
     e.preventDefault();
 fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${searchAccion.value}&apikey=1F5ADZP9JKXXORG5`)
 .then((resultado) => resultado.json())
-.then ((respuesta) => {
-    console.log(respuesta);
-    const element = document.createElement("div");
-    element.classList.add("card");
-    element.innerHTML = `<h2 class="accion-nombre">${respuesta["Global Quote"]["01. symbol"]}</h2>
+.then ((respuesta) => {   
+    results.classList.add("card");
+    results.innerHTML = `<h2 class="accion-nombre">${respuesta["Global Quote"]["01. symbol"]}</h2>
     <h3>Precio:</h3> <p>${respuesta["Global Quote"]["05. price"]}</p>
     <h3>Variación diaria:</h3> <p class>${respuesta["Global Quote"]["10. change percent"]}</p>`;
-    console.log(element);
-    contenedor.appendChild(element);
 })
 })
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //Declaro la array vacía
-let portafolio = [];
+let portafolio = JSON.parse(localStorage.getItem("portafolio")) || [];
 //Declaro las variables utilizando DOM y el id
 const formulario = document.getElementById("formulario");
 const nombre = document.getElementById("nombreAccion");
 const costo = document.getElementById("costo");
 const precio = document.getElementById("precio");
 const tabla = document.getElementById("tabla");
+const body = document.getElementById("body");
+
+
+document.addEventListener("DOMContentLoaded", tablaPorta);
+
 
 //Le agrego un evento para que escuche cuando el usuario hace submit al formulario
 formulario.addEventListener("submit", function (e) {
@@ -22,38 +27,7 @@ formulario.addEventListener("submit", function (e) {
   //Guardo la nueva array en el local storage
   localStorage.setItem("portafolio", JSON.stringify(portafolio));
 
-  //Agrego un header a la tabla ya existente
-  tabla.innerHTML = `  <thead>
-  <tr>
-    <th scope="col">Nombre</th>
-    <th scope="col">Precio de compra</th>
-    <th scope="col">Cotización del día</th>
-    <th scope="col">Rendimiento</th>
-  </tr>
-</thead>
-<tbody id = "body">
-</tbody>`;
 
-  const body = document.getElementById("body");
-  //Llamo la información del local Storage y la parseo
-  const portafolioLocalStorage = JSON.parse(localStorage.getItem("portafolio"));
-
-  //Por cada acción que el usuario agrega, se agrega una fila a la tabla utilizando los elementos de la array
-  for (const accion of portafolioLocalStorage) {
-    let fila = document.createElement("tr"); //Creo la fila
-
-    fila.innerHTML = `<td>${accion.nombre}</td> 
-  <td>${accion.costo}</td>
-  <td>${accion.precio}</td>
-  <td id ="${accion.nombre}">${accion.rendimiento}</td>`;
-
-  
-  body.appendChild(fila);
-  
-  //Utilizando un operador ternario, le determino la clase a al texto de la columna rendimiento dependiendo si el mismo es positivo o negativo
-  parseInt(accion.rendimiento) > 0 ? document.getElementById(accion.nombre).className = "rendPositivo" : document.getElementById(accion.nombre).className = "rendNegativo";
- 
-  }
   Toastify({
     text: "Has agregado una acción a tu portafolio",
     duration: 2000,
@@ -64,3 +38,21 @@ formulario.addEventListener("submit", function (e) {
     }
 }).showToast();
 })
+
+function tablaPorta() {
+  if (portafolio.length === 0) {
+    tabla.className = "ocultar";
+  } else {
+    for(i=0; i<portafolio.length; i++){
+      let r = body.insertRow();
+      let cell1=r.insertCell();
+      let cell2=r.insertCell();
+      let cell3=r.insertCell();
+      let cell4=r.insertCell();
+
+      cell1.innerHTML=portafolio[i].nombre;
+      cell2.innerHTML=portafolio[i].costo;
+      cell3.innerHTML=portafolio[i].precio;
+      cell4.innerHTML=portafolio[i].rendimiento;
+    }
+  }};
